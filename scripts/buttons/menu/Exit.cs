a2ff8db7c51e775b21fab8412dab99152184791f
KokoT <prikolkillercat@gmail.com>
2025-02-14 13:31:22 +0100

    - menu buttons sound onpress

--- scripts/buttons/menu/Exit.cs
+++ scripts/buttons/menu/Exit.cs
@@ -3,13 +3,26 @@ using System;
 
 public partial class Exit : Button
 {
+    AudioStream _clickSound = GD.Load<AudioStream>("res://sounds/tick.ogg");
+    AudioStreamPlayer _player;
+
     public override void _Ready()
     {
+        _player = new AudioStreamPlayer();
+        AddChild(_player);
+
         Connect("button_up", new Callable(this, nameof(OnRelease)));
+        Connect("button_down", new Callable(this, nameof(OnPress)));
     }
 
     private void OnRelease()
     {
         GetTree().Quit();
+    }
+
+    private void OnPress()
+    {
+        _player.Stream = _clickSound;
+        _player.Play();
     }
 }
--- scripts/buttons/menu/Play.cs
+++ scripts/buttons/menu/Play.cs
@@ -3,14 +3,27 @@ using System;
 
 public partial class Play : Button
 {
+    AudioStream _clickSound = GD.Load<AudioStream>("res://sounds/tick.ogg");
+    AudioStreamPlayer _player;
+
     public override void _Ready()
     {
+        _player = new AudioStreamPlayer();
+        AddChild(_player);
+
         Connect("button_up", new Callable(this, nameof(OnRelease)));
+        Connect("button_down", new Callable(this, nameof(OnPress)));
     }
 
     private void OnRelease()
     {
         GD.Print("loading game scene");
         GetTree().ChangeSceneToFile("res://scenes/game.tscn");
+    }
+
+    private void OnPress()
+    {
+        _player.Stream = _clickSound;
+        _player.Play();
     }
 }
